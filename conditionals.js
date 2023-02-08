3ae9677d000814892d6bda3dfbb5ef089c3715df
iuf7 <iuf7@fresnou.org>
2023-02-08 13:32:49 -0800

    initial commit

---
+++ conditionals.js
@@ -0,0 +1,79 @@
+// Conditionals are if-statements
+
+let x = 10;
+if (x==5){
+    console.log("its 5!")
+}
+console.log("end of program");
+
+let z = 20;
+if (z==5) {
+    console.log("optional");
+}else{
+    console.log("optional2");
+}
+console.log("end of program");
+
+
+let w = 20;
+if (w == 20){
+    console.log("optional");
+}else{
+    console.log(optional2);
+}
+console.log("end of program");
+
+
+
+let num1 = 100;
+if (num1 == 10){
+    console.log("it's 10");
+} else if (num1 == 6) {
+    console.log("it's 6");
+} else {
+    console.log("it's something else");
+
+}
+console.log("end of program");
+
+let num2 = 6;
+if (num2 == 10){
+    console.log("equals 10");
+} else if (num2>6) {
+    console.log("it's something else");
+}
+console.log("end of program");
+
+let secret = 7
+if (secret % 7==0){
+    console.log("divisible by 7");
+} else if (secret % 3==0){
+    console.log("divisible by 3");
+}
+console.log("end of program");
+
+let secret = 5
+if (secret % 7==0){
+    console.log("divisible by 7");
+} else if (secret % 3==0){
+    console.log("divisible by 3");
+}
+console.log("end of program");
+
+let rand = 3.4567;
+if (rand > 1);
+console.log("greater than 1");{
+}
+if (rand > 2);
+console.log("greater than 2");{
+}
+console.log("end of program");
+
+
+let x = 5;
+if (x > 1){
+    console.log("greater");
+} else {
+    console.log("not greater");
+}
+console.log("end of program");
