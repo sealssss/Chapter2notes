7578df0d6379e2591617346865ca155f9b2216df
iuf7 <iuf7@fresnou.org>
2023-02-15 13:49:05 -0800

    initial commit

--- inputs1.js
+++ inputs1.js
@@ -6,14 +6,17 @@ console.log(answer);
 if (answer="yes") {
 console.log("chicken butt")
 }
-const readline = require('readline-sync');let number = readline.question('give me any number of your choice \nyour answer: ')
-;if (number % 2 == 0) {
+const readline = require('readline-sync');
+
+let number = readline.question('give me any number of your choice \nyour answer: ');
+if (number % 2 == 0) {
     console.log("your number is divisible by 2")
 }else
         {
     console.log("its not divisible by 2")
         }
     console.log("end of program");
+
 const readline = require('readline-sync');
 
 let number = readline.question('give me any number of your choice \nyour answer: ')
@@ -23,35 +26,40 @@ let number = readline.question('give me any number of your choice \nyour answer:
 console.log("end of program")
 
 if (number % 3 == 0) {
-    console.log("your number is divisible by 2")
+    console.log("your number is divisible by 3")
 }else {
-    console.log("its not divisible by 2")
+    console.log("its not divisible by 3")
 }
 console.log("end of program")
 
 if (number % 4 == 0) {
-    console.log("your number is divisible by 2")
+    console.log("your number is divisible by 4")
 }else {
-    console.log("its not divisible by 2")
+    console.log("its not divisible by 4")
 }
 console.log("end of program")
 
 if (number % 5 == 0) {
-    console.log("your number is divisible by 2")
+    console.log("your number is divisible by 5")
 }else {
-    console.log("its not divisible by 2")
+    console.log("its not divisible by 5")
 }
 console.log("end of program")
 
 if (number % 6 == 0) {
-    console.log("your number is divisible by 2")
+    console.log("your number is divisible by 6")
 }else {
-    console.log("its not divisible by 2")
+    console.log("its not divisible by 6")
 }
 console.log("end of program")
 
 const readLine = require ('readline-sync');
 
-let answer = readLine.question("Guess My Number");
-console.log(answer)
+let guess = readLine.question("Guess My Number");
+if (guess == 20);
+console.log("You Won!");
 
+
+
+
+let cost = Number(readLine.question("Enter cost: ");
